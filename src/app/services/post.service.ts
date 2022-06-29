b217e9738409497e1a6452a1eec194a0073f3ae2
Puneet Sandhu <pssarhali@gmail.com>
2022-06-28 22:09:24 -0700

    dummy data loaded

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Post } from 'src/app/models/post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  postList: Post[] = [
+    {
+      postid: "1",
+      projectName: "Open Collab",
+      description: "A collaboration tool for open source developers to propose new project ideas, collaborate with developers interested in your proposal and transparently keep track of the project’s progress.",
+      score: 26
+    },
+    {
+      postid: "2",
+      projectName: "Weather API",
+      description: "A collaboration tool for open source developers to propose new project ideas, collaborate with developers interested in your proposal and transparently keep track of the project’s progress.",
+      score: 45
+    },
+    {
+      postid: "3",
+      projectName: "Angular UI Library",
+      description: "A collaboration tool for open source developers to propose new project ideas, collaborate with developers interested in your proposal and transparently keep track of the project’s progress.",
+      score: 97
+    }
+  ]
+
+  getPostList(){
+    return this.postList;
+  }
+  constructor() { }
+}
